5f93fe4cd40e0195d005aff1ccebbc175fefc6c6
mswift42 <martin.haesler@gmail.com>
2016-04-19 19:16:05 +0100

    Import MdButton/ MdList

--- src/client/app/liveres.ts
+++ src/client/app/liveres.ts
@@ -1,11 +1,15 @@
 import {Component} from 'angular2/core';
 import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
+import {MdToolbar} from '@angular2-material/toolbar';
+import {MdSidenav} from '@angular2-material/sidenav';
+import {MdList} from '@angular2-material/list';
+import {MdButton} from '@angular2-material/button';
 
 @Component({
   selector: 'liveres-app',
   providers: [ROUTER_PROVIDERS],
   templateUrl: 'app/liveres.html',
-  directives: [ROUTER_DIRECTIVES],
+  directives: [ROUTER_DIRECTIVES, MdToolbar],
   pipes: []
 })
 @RouteConfig([
