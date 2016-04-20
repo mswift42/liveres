2ae468e208aaf05a1e6a9f92cd21a1892a632d9e
mswift42 <martin.haesler@gmail.com>
2016-04-20 18:22:47 +0100

    Generate component resume-cat

---
+++ src/client/app/resume-cat/resume-cat.spec.ts
@@ -0,0 +1,26 @@
+import {
+  it,
+  iit,
+  describe,
+  ddescribe,
+  expect,
+  inject,
+  injectAsync,
+  TestComponentBuilder,
+  beforeEachProviders
+} from 'angular2/testing';
+import {provide} from 'angular2/core';
+import {ResumeCat} from './resume-cat';
+
+describe('ResumeCat Component', () => {
+
+  beforeEachProviders((): any[] => []);
+
+
+  it('should ...', injectAsync([TestComponentBuilder], (tcb:TestComponentBuilder) => {
+    return tcb.createAsync(ResumeCat).then((fixture) => {
+      fixture.detectChanges();
+    });
+  }));
+
+});
---
+++ src/client/app/resume-cat/resume-cat.ts
@@ -0,0 +1,16 @@
+import {Component} from 'angular2/core';
+
+
+@Component({
+  selector: 'resume-cat',
+  templateUrl: 'app//resume-cat/resume-cat.html',
+  styleUrls: ['app//resume-cat/resume-cat.css'],
+  providers: [],
+  directives: [],
+  pipes: []
+})
+export class ResumeCat {
+
+  constructor() {}
+
+}
