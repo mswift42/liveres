1145201183c202bf470a097b9563baff084ccc98
mswift42 <martin.haesler@gmail.com>
2016-04-22 21:04:45 +0100

    fix icon spellings

--- src/client/app/sidenavservice/sidenavservice.ts
+++ src/client/app/sidenavservice/sidenavservice.ts
@@ -12,11 +12,11 @@ export class SidenavService {
     constructor() {}
     getCategories() {
         return [
-            new SidenavCategory("contact", "account circle"),
+            new SidenavCategory("contact", "account_circle"),
             new SidenavCategory("profile", "face"),
             new SidenavCategory("projects", "history"),
-            new SidenavCategory("employment", "card travel"),
-            new SidenavCategory("education", "question answer"),
+            new SidenavCategory("employment", "card_travel"),
+            new SidenavCategory("education", "question_answer"),
             new SidenavCategory("awards", "stars")
         ]
     }
