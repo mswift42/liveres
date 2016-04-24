d490a315cb71cd4b9b3fa44f6e8de30a94201f4f
mswift42 <martin.haesler@gmail.com>
2016-04-24 14:13:38 +0100

    downcase education field

--- src/client/app/res-service/res-service.ts
+++ src/client/app/res-service/res-service.ts
@@ -20,8 +20,8 @@ class Employment {
 }
 
 class Eductaion {
-  constructor(private period: String, private name: String,
-    private details: String) { }
+  constructor(private period: String, private institution: String,
+    private degree: String, private details: String) { }
 }
 
 class Skill {
@@ -42,7 +42,7 @@ class Resume {
   public contact: ContactSection;
   public projects: Project[];
   public employment: Employment[];
-  public Eductaion: Eductaion[];
+  public education: Eductaion[];
   public skills: Skill[];
   public languages: Language[];
   public awards: Award[];
@@ -57,8 +57,22 @@ export class ResService {
   
   public getSampleResume(): Resume {
     let res = new Resume();
-    res.about = " Richard hails from Tulsa. He has earned degrees from the University of Oklahoma and Stanford. (Go Sooners and Cardinals!) Before starting Pied Piper, he worked for Hooli as a part time software developer. While his work focuses on applied information theory, mostly optimizing lossless compression schema of both the length-limited and adaptive variants, his non-work interests range widely, everything from quantum computing to chaos theory. He could tell you about it, but THAT would NOT be a “length-limited” conversation! "
-    
+    res.about = " Richard hails from Tulsa. He has earned degrees from the University of Oklahoma and Stanford. (Go Sooners and Cardinals!) Before starting Pied Piper, he worked for Hooli as a part time software developer. While his work focuses on applied information theory, mostly optimizing lossless compression schema of both the length-limited and adaptive variants, his non-work interests range widely, everything from quantum computing to chaos theory. He could tell you about it, but THAT would NOT be a “length-limited” conversation! ";
+    res.contact = new ContactSection("richard.hendriks@mail.com",
+     "(912) 555-4321", "http://richardhendriks.com");
+     res.projects = [
+       new Project("todoapp1", "sophisticated todo app.",
+       "blowyourmind.png")
+     ];
+     res.employment = [
+       new Employment("December 2013 - December 2014",
+        "Pied Piper", "CEO/President",
+        " Richard hails from Tulsa. He has earned degrees from the University of Oklahoma and Stanford. (Go Sooners and Cardinals!) Before starting Pied Piper, he worked for Hooli as a part time software developer. While his work focuses on applied information theory, mostly optimizing lossless compression schema of both the length-limited and adaptive variants, his non-work interests range widely, everything from quantum computing to chaos theory. He could tell you about it, but THAT would NOT be a “length-limited” conversation! ")
+     ];
+     res.education = [
+       new Eductaion("2011 - 2014", "University of Oklahoma",
+       "Bachelor, Information Technology", "" )
+     ];
   }
 
 }
