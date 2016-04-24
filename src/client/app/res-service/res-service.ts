cb55c679c385ce41f05a9d0ddbe2a32fe2324499
mswift42 <martin.haesler@gmail.com>
2016-04-24 10:53:18 +0100

    Add class Education and Skill

--- src/client/app/res-service/res-service.ts
+++ src/client/app/res-service/res-service.ts
@@ -2,21 +2,30 @@ import {Injectable} from 'angular2/core';
 
 class ContactSection {
   constructor(private email: String, private phone: String,
-  private website: String) {}
+    private website: String) { }
 }
 
-class Profile{
-  constructor(private company: String, private url: String) {}
+class Profile {
+  constructor(private company: String, private url: String) { }
 }
 
 class Project {
   constructor(private title: String, private description: String,
-  private screenshot: String)  {}
+    private screenshot: String) { }
 }
 
 class Employment {
   constructor(private period: String, private company: String,
-  private position: String, private description: String) {}
+    private position: String, private description: String) { }
+}
+
+class Eductaion {
+  constructor(private period: String, private name: String,
+    private details: String) { }
+}
+
+class Skill {
+  constructor (private title: String, private details: String[]) {}
 }
 
 class Resume {
@@ -28,6 +37,6 @@ class Resume {
 @Injectable()
 export class ResService {
 
-  constructor() {}
+  constructor() { }
 
 }
