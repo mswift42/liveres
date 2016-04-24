c984fe9d8f5c636dd3900f0fc6432d769cdb1dd1
mswift42 <martin.haesler@gmail.com>
2016-04-24 17:32:21 +0100

    Import MD_CARD_Directives

--- src/client/app/section-component/section-component.ts
+++ src/client/app/section-component/section-component.ts
@@ -1,4 +1,5 @@
 import {Component} from 'angular2/core';
+import {MD_CARD_DIRECTIVES} from '@angular2-material/card';
 
 
 @Component({
@@ -6,7 +7,7 @@ import {Component} from 'angular2/core';
   templateUrl: 'app//section-component/section-component.html',
   styleUrls: ['app//section-component/section-component.css'],
   providers: [],
-  directives: [],
+  directives: [MD_CARD_DIRECTIVES],
   pipes: []
 })
 export class SectionComponent {
