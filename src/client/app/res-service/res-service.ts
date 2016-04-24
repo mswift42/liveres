64664165be2e11bc88bc27312d8ef429bcde1e34
mswift42 <martin.haesler@gmail.com>
2016-04-24 10:39:00 +0100

    Add contactsection

--- src/client/app/res-service/res-service.ts
+++ src/client/app/res-service/res-service.ts
@@ -1,5 +1,15 @@
 import {Injectable} from 'angular2/core';
 
+class ContactSection {
+  constructor(private email: String, private phone: String,
+  private website: String) {}
+}
+
+class Resume {
+  private about: String;
+  private contact: 
+}
+
 
 @Injectable()
 export class ResService {
