8671a6883db17da05e4604b906c89015d54f707e
mswift42 <martin.haesler@gmail.com>
2016-04-24 10:26:17 +0100

    Generate new component section-component

---
+++ src/client/app/section-component/section-component.ts
@@ -0,0 +1,16 @@
+import {Component} from 'angular2/core';
+
+
+@Component({
+  selector: 'section-component',
+  templateUrl: 'app//section-component/section-component.html',
+  styleUrls: ['app//section-component/section-component.css'],
+  providers: [],
+  directives: [],
+  pipes: []
+})
+export class SectionComponent {
+
+  constructor() {}
+
+}
