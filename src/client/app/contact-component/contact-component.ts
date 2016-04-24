28446318577ba3e732e983a93a59fbc16cdb9af3
mswift42 <martin.haesler@gmail.com>
2016-04-24 18:16:27 +0100

    Generate contact component

---
+++ src/client/app/contact-component/contact-component.ts
@@ -0,0 +1,16 @@
+import {Component} from 'angular2/core';
+
+
+@Component({
+  selector: 'contact-component',
+  templateUrl: 'app//contact-component/contact-component.html',
+  styleUrls: ['app//contact-component/contact-component.css'],
+  providers: [],
+  directives: [],
+  pipes: []
+})
+export class ContactComponent {
+
+  constructor() {}
+
+}
