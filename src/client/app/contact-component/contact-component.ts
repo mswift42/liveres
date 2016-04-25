9a26788e610de2b376fb5f94064c659263aabdf5
mswift42 <martin.haesler@gmail.com>
2016-04-25 13:09:26 +0100

    Add Inputs for mail,phone,url.

--- src/client/app/contact-component/contact-component.ts
+++ src/client/app/contact-component/contact-component.ts
@@ -1,4 +1,7 @@
-import {Component} from 'angular2/core';
+import {Component, Input} from 'angular2/core';
+import {SectionComponent} from '../section-component/section-component';
+import {MD_CARD_DIRECTIVES} from '@angular2-material/card';
+
 
 
 @Component({
@@ -6,10 +9,13 @@ import {Component} from 'angular2/core';
   templateUrl: 'app//contact-component/contact-component.html',
   styleUrls: ['app//contact-component/contact-component.css'],
   providers: [],
-  directives: [],
+  directives: [SectionComponent, MD_CARD_DIRECTIVES],
   pipes: []
 })
 export class ContactComponent {
+  @Input() email: String;
+  @Input() phone: String;
+  @Input() website: String;
 
   constructor() {}
 
