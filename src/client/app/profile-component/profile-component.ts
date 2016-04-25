d3ed36900a9bf989ac93454855c794daaa2ba677
mswift42 <martin.haesler@gmail.com>
2016-04-25 18:06:29 +0100

    Generate profile component

---
+++ src/client/app/profile-component/profile-component.ts
@@ -0,0 +1,16 @@
+import {Component} from 'angular2/core';
+
+
+@Component({
+  selector: 'profile-component',
+  templateUrl: 'app//profile-component/profile-component.html',
+  styleUrls: ['app//profile-component/profile-component.css'],
+  providers: [],
+  directives: [],
+  pipes: []
+})
+export class ProfileComponent {
+
+  constructor() {}
+
+}
