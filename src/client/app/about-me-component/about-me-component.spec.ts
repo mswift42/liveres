5d61405f9beb367a5dd163e8cf782f4ade2d1bc6
mswift42 <martin.haesler@gmail.com>
2016-04-25 20:03:39 +0100

    Generate new component about-me-component

---
+++ src/client/app/about-me-component/about-me-component.spec.ts
@@ -0,0 +1,26 @@
+import {
+  it,
+  iit,
+  describe,
+  ddescribe,
+  expect,
+  inject,
+  injectAsync,
+  TestComponentBuilder,
+  beforeEachProviders
+} from 'angular2/testing';
+import {provide} from 'angular2/core';
+import {AboutMe} from './about-me';
+
+describe('AboutMe Component', () => {
+
+  beforeEachProviders((): any[] => []);
+
+
+  it('should ...', injectAsync([TestComponentBuilder], (tcb:TestComponentBuilder) => {
+    return tcb.createAsync(AboutMe).then((fixture) => {
+      fixture.detectChanges();
+    });
+  }));
+
+});
---
+++ src/client/app/about-me-component/about-me-component.ts
@@ -0,0 +1,16 @@
+import {Component} from 'angular2/core';
+
+
+@Component({
+  selector: 'about-me',
+  templateUrl: 'app//about-me/about-me.html',
+  styleUrls: ['app//about-me/about-me.css'],
+  providers: [],
+  directives: [],
+  pipes: []
+})
+export class AboutMe {
+
+  constructor() {}
+
+}
