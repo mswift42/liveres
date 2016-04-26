44b1bfd513dbad5ec238896fc03073415226f99f
mswift42 <martin.haesler@gmail.com>
2016-04-26 15:46:55 +0100

    Generate project-component

---
+++ src/client/app/project-component/project-component.ts
@@ -0,0 +1,16 @@
+import {Component} from 'angular2/core';
+
+
+@Component({
+  selector: 'project-component',
+  templateUrl: 'app//project-component/project-component.html',
+  styleUrls: ['app//project-component/project-component.css'],
+  providers: [],
+  directives: [],
+  pipes: []
+})
+export class ProjectComponent {
+
+  constructor() {}
+
+}
