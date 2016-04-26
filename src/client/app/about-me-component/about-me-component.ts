57ac1b912493f2e2f29db852fa5635f686d2de7e
mswift42 <martin.haesler@gmail.com>
2016-04-26 14:23:41 +0100

    Import SectionComponent, md-card

--- src/client/app/about-me-component/about-me-component.ts
+++ src/client/app/about-me-component/about-me-component.ts
@@ -1,16 +1,22 @@
 import {Component} from 'angular2/core';
+import {SectionComponent} from '../section-component/section-component';
+import {Resume, ResService} from '../res-service/res-service';
+import {MD_CARD_DIRECTIVES} from '@angular2-material/card';
 
 
 @Component({
   selector: 'about-me',
-  templateUrl: 'app//about-me/about-me.html',
-  styleUrls: ['app//about-me/about-me.css'],
+  templateUrl: 'app//about-me-component/about-me-component.html',
+  styleUrls: ['app//about-me-component/about-me-component.css'],
   providers: [],
-  directives: [],
+  directives: [SectionComponent, MD_CARD_DIRECTIVES],
   pipes: []
 })
 export class AboutMe {
+    abouttext: String;
 
-  constructor() {}
+  constructor(resservice: ResService) {
+    this.abouttext = resservice.getSampleResume().about;
+  }
 
 }
