7c62b256e7e44b12adc376896d7c8b6bb2e19574
mswift42 <martin.haesler@gmail.com>
2016-04-29 12:51:37 +0100

    Generate educations component

---
+++ src/client/app/educations-component/educations-component.ts
@@ -0,0 +1,16 @@
+import {Component} from 'angular2/core';
+
+
+@Component({
+  selector: 'educations-component',
+  templateUrl: 'app//educations-component/educations-component.html',
+  styleUrls: ['app//educations-component/educations-component.css'],
+  providers: [],
+  directives: [],
+  pipes: []
+})
+export class EducationsComponent {
+
+  constructor() {}
+
+}
