ff46a4f18b6cae174aef244587088e9438fef874
mswift42 <martin.haesler@gmail.com>
2016-04-29 13:12:50 +0100

    Generate employment component

---
+++ src/client/app/employment-component/employment-component.ts
@@ -0,0 +1,16 @@
+import {Component} from 'angular2/core';
+
+
+@Component({
+  selector: 'employment-component',
+  templateUrl: 'app//employment-component/employment-component.html',
+  styleUrls: ['app//employment-component/employment-component.css'],
+  providers: [],
+  directives: [],
+  pipes: []
+})
+export class EmploymentComponent {
+
+  constructor() {}
+
+}
