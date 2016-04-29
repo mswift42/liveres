644e0551938f6ffb3ebc786dc9965684f8b95756
mswift42 <martin.haesler@gmail.com>
2016-04-29 12:14:23 +0100

    Generate projects component

---
+++ src/client/app/projects-component/projects-component.ts
@@ -0,0 +1,16 @@
+import {Component} from 'angular2/core';
+
+
+@Component({
+  selector: 'projects-component',
+  templateUrl: 'app//projects-component/projects-component.html',
+  styleUrls: ['app//projects-component/projects-component.css'],
+  providers: [],
+  directives: [],
+  pipes: []
+})
+export class ProjectsComponent {
+
+  constructor() {}
+
+}
