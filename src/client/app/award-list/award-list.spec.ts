6fcf62e0c70f399e87bbf77e3ff59304c2b8d665
mswift42 <martin.haesler@gmail.com>
2016-04-29 17:45:13 +0100

    Add award list

---
+++ src/client/app/award-list/award-list.spec.ts
@@ -0,0 +1,26 @@
+import {
+  it,
+  iit,
+  describe,
+  ddescribe,
+  expect,
+  inject,
+  injectAsync,
+  TestComponentBuilder,
+  beforeEachProviders
+} from 'angular2/testing';
+import {provide} from 'angular2/core';
+import {AwardList} from './award-list';
+
+describe('AwardList Component', () => {
+
+  beforeEachProviders((): any[] => []);
+
+
+  it('should ...', injectAsync([TestComponentBuilder], (tcb:TestComponentBuilder) => {
+    return tcb.createAsync(AwardList).then((fixture) => {
+      fixture.detectChanges();
+    });
+  }));
+
+});
