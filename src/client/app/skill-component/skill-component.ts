d1e6c610e282b1a0f9f79b711a5b4ecf1a085688
mswift42 <martin.haesler@gmail.com>
2016-04-29 14:54:40 +0100

    Generate skill component

---
+++ src/client/app/skill-component/skill-component.ts
@@ -0,0 +1,16 @@
+import {Component} from 'angular2/core';
+
+
+@Component({
+  selector: 'skill-component',
+  templateUrl: 'app//skill-component/skill-component.html',
+  styleUrls: ['app//skill-component/skill-component.css'],
+  providers: [],
+  directives: [],
+  pipes: []
+})
+export class SkillComponent {
+
+  constructor() {}
+
+}
