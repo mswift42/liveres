f8051e8009f96e3d098e53807472c65988d350ca
mswift42 <martin.haesler@gmail.com>
2016-04-29 17:27:34 +0100

    add language list

---
+++ src/client/app/language-list/language-list.spec.ts
@@ -0,0 +1,26 @@
+import {
+  it,
+  iit,
+  describe,
+  ddescribe,
+  expect,
+  inject,
+  injectAsync,
+  TestComponentBuilder,
+  beforeEachProviders
+} from 'angular2/testing';
+import {provide} from 'angular2/core';
+import {LanguageList} from './language-list';
+
+describe('LanguageList Component', () => {
+
+  beforeEachProviders((): any[] => []);
+
+
+  it('should ...', injectAsync([TestComponentBuilder], (tcb:TestComponentBuilder) => {
+    return tcb.createAsync(LanguageList).then((fixture) => {
+      fixture.detectChanges();
+    });
+  }));
+
+});
