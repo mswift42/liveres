c2f6a0ac5e48756792fd22c3d166142d99ad8505
mswift42 <martin.haesler@gmail.com>
2016-04-29 13:33:18 +0100

    create new component employment-list

---
+++ src/client/app/employment-list/employment-list.spec.ts
@@ -0,0 +1,26 @@
+import {
+  it,
+  iit,
+  describe,
+  ddescribe,
+  expect,
+  inject,
+  injectAsync,
+  TestComponentBuilder,
+  beforeEachProviders
+} from 'angular2/testing';
+import {provide} from 'angular2/core';
+import {EmploymentList} from './employment-list';
+
+describe('EmploymentList Component', () => {
+
+  beforeEachProviders((): any[] => []);
+
+
+  it('should ...', injectAsync([TestComponentBuilder], (tcb:TestComponentBuilder) => {
+    return tcb.createAsync(EmploymentList).then((fixture) => {
+      fixture.detectChanges();
+    });
+  }));
+
+});
