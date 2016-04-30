fa31d6fef268776f93516632ab16922a2cfaaa3c
mswift42 <martin.haesler@gmail.com>
2016-04-30 11:03:42 +0100

    Generate image component

---
+++ src/client/app/image-component/image-component.ts
@@ -0,0 +1,16 @@
+import {Component} from 'angular2/core';
+
+
+@Component({
+  selector: 'image-component',
+  templateUrl: 'app//image-component/image-component.html',
+  styleUrls: ['app//image-component/image-component.css'],
+  providers: [],
+  directives: [],
+  pipes: []
+})
+export class ImageComponent {
+
+  constructor() {}
+
+}
