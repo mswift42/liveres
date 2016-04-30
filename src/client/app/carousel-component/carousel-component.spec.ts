9fab8bc7c599a1fcf2da4bf9568cb6381c1869b8
mswift42 <martin.haesler@gmail.com>
2016-04-30 09:38:45 +0100

    Generate carousel-component

---
+++ src/client/app/carousel-component/carousel-component.spec.ts
@@ -0,0 +1,26 @@
+import {
+  it,
+  iit,
+  describe,
+  ddescribe,
+  expect,
+  inject,
+  injectAsync,
+  TestComponentBuilder,
+  beforeEachProviders
+} from 'angular2/testing';
+import {provide} from 'angular2/core';
+import {CarouselComponent} from './carousel-component';
+
+describe('CarouselComponent Component', () => {
+
+  beforeEachProviders((): any[] => []);
+
+
+  it('should ...', injectAsync([TestComponentBuilder], (tcb:TestComponentBuilder) => {
+    return tcb.createAsync(CarouselComponent).then((fixture) => {
+      fixture.detectChanges();
+    });
+  }));
+
+});
---
+++ src/client/app/carousel-component/carousel-component.ts
@@ -0,0 +1,16 @@
+import {Component} from 'angular2/core';
+
+
+@Component({
+  selector: 'carousel-component',
+  templateUrl: 'app//carousel-component/carousel-component.html',
+  styleUrls: ['app//carousel-component/carousel-component.css'],
+  providers: [],
+  directives: [],
+  pipes: []
+})
+export class CarouselComponent {
+
+  constructor() {}
+
+}
