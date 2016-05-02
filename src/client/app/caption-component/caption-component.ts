ce16ffab1c370daef442b0231da76f7c1a8ec96a
mswift42 <martin.haesler@gmail.com>
2016-05-02 10:27:28 +0100

    Generate caption component

---
+++ src/client/app/caption-component/caption-component.ts
@@ -0,0 +1,16 @@
+import {Component} from 'angular2/core';
+
+
+@Component({
+  selector: 'caption-component',
+  templateUrl: 'app//caption-component/caption-component.html',
+  styleUrls: ['app//caption-component/caption-component.css'],
+  providers: [],
+  directives: [],
+  pipes: []
+})
+export class CaptionComponent {
+
+  constructor() {}
+
+}
