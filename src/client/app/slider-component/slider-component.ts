49788b39b2025e937d967187619481cbb8d1159d
mswift42 <martin.haesler@gmail.com>
2016-05-02 19:02:46 +0100

    add method isActiveIndex

--- src/client/app/slider-component/slider-component.ts
+++ src/client/app/slider-component/slider-component.ts
@@ -1,5 +1,8 @@
 import {Component} from 'angular2/core';
 import {MD_CARD_DIRECTIVES} from '@angular2-material/card';
+import {ImageComponent} from '../image-component/image-component';
+import {SliderImage} from '../slider-service/slider-service';
+import {CaptionComponent} from '../caption-component/caption-component';
 
 
 @Component({
@@ -7,11 +10,17 @@ import {MD_CARD_DIRECTIVES} from '@angular2-material/card';
   templateUrl: 'app//slider-component/slider-component.html',
   styleUrls: ['app//slider-component/slider-component.css'],
   providers: [],
-  directives: [MD_CARD_DIRECTIVES],
+  directives: [MD_CARD_DIRECTIVES, ImageComponent,
+    CaptionComponent],
   pipes: []
 })
 export class SliderComponent {
+  images: SliderImage[];
+  activeIndex: number = 0;
 
-  constructor() {}
-
+  constructor() { } 
+  
+  isActiveIndex(i: number) : boolean {
+    return i === this.activeIndex;
+  }
 }
