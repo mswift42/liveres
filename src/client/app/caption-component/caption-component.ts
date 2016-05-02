a2753dc593665530446711ec96acd3a5ee3bd471
mswift42 <martin.haesler@gmail.com>
2016-05-02 20:06:10 +0100

    Add input properties.

--- src/client/app/caption-component/caption-component.ts
+++ src/client/app/caption-component/caption-component.ts
@@ -1,4 +1,4 @@
-import {Component} from 'angular2/core';
+import {Component, Input} from 'angular2/core';
 
 
 @Component({
@@ -10,6 +10,8 @@ import {Component} from 'angular2/core';
   pipes: []
 })
 export class CaptionComponent {
+ @Input() captiontitle: String;
+ @Input() captiontext: String;
 
   constructor() {}
 
