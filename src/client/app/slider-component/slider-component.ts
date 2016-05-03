6e4ea1805c8c6d1ac4a00045c57a234368ce265f
mswift42 <martin.haesler@gmail.com>
2016-05-03 14:51:18 +0100

    Wrap inside section-component

--- src/client/app/slider-component/slider-component.ts
+++ src/client/app/slider-component/slider-component.ts
@@ -3,6 +3,7 @@ import {MD_CARD_DIRECTIVES} from '@angular2-material/card';
 import {ImageComponent} from '../image-component/image-component';
 import {SliderImage, SliderService} from '../slider-service/slider-service';
 import {CaptionComponent} from '../caption-component/caption-component';
+import {SectionComponent} from '../section-component/section-component';
 
 
 @Component({
@@ -11,7 +12,7 @@ import {CaptionComponent} from '../caption-component/caption-component';
   styleUrls: ['app//slider-component/slider-component.css'],
   providers: [SliderService],
   directives: [MD_CARD_DIRECTIVES, ImageComponent,
-    CaptionComponent],
+    CaptionComponent, SectionComponent],
   pipes: []
 })
 export class SliderComponent {
