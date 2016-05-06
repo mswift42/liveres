ebc8b3d44817170aa5ec7f7595413ea8a064c14e
mswift42 <martin.haesler@gmail.com>
2016-05-06 17:17:43 +0100

    update to alpha4

--- angular-cli-build.js
+++ angular-cli-build.js
@@ -4,7 +4,16 @@ var Angular2App = require('angular-cli/lib/broccoli/angular2-app');
 
 module.exports = function(defaults) {
   var app = new Angular2App(defaults, {
-    vendorNpmFiles: ['@angular2-material/**/*.js']
+      vendorNpmFiles: [
+          '@angular2-material/**/*.js',
+          'systemjs/dist/system-polyfills.js',
+          'systemjs/dist/system.src.js',
+          'zone.js/dist/*.js',
+          'es6-shim/es6-shim.js',
+          'reflect-metadata/*.js',
+          'rxjs/**/*.js',
+          '@angular/**/*.js'
+      ]
   });
-  return app.toTree();
+    return app.toTree();
 };
